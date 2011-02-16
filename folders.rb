093368b2e80c40c9ddf1bf085da972ee728f7203
Stian Haklev <shaklev@gmail.com>
2011-02-16 12:54:46 -0500

    all working, clean paths etc

--- folders.rb
+++ folders.rb
@@ -2,28 +2,40 @@ require 'find'
 require 'fileutils'
 require './library'
 
+#######################################################################
 searchpath = "MA/"  
 TITLE = "MA thesis"
 OUT = "/Users/stian/src/kindle/out/"
 layout = 2
+#######################################################################
+
+def rel_path(path)
+  path[OUT.size..-1]
+end
 
 def sanitize_filename(filename)
   name = filename.strip 
-  # NOTE: File.basename doesn't work right with Windows paths on Unix
-  # get only the filename, not the whole path
-  name.gsub! /^.*(\\|\/)/, ''
-
-  # Finally, replace all non alphanumeric, underscore 
-  # or periods with underscore
-  # name.gsub! /[^\w\.\-]/, '_'
-  # Basically strip out the non-ascii alphabets too 
-  # and replace with x. 
-  # You don't want all _ :)
+  name.gsub!(/^.*(\\|\/)/, '')
   name.gsub!(/[^0-9A-Za-z.\-]/, '_')
   return name
 end
 
-def create_index(path)
+def skip?(filename)
+  true if ['index.html','Images'].index(filename)
+end
+
+def clean_name(path) # takes full path, moves file, returns new rel path and display name
+  name = File.basename(path)
+  onlypath = path[0..-name.size-2]
+  onlypath = onlypath + "/" unless onlypath[-1..-1] == "/"
+  clean = sanitize_filename(name)
+  cleanpath = onlypath + clean
+  FileUtils.mv(path, cleanpath) unless name == clean
+  shortname = name.gsub(".html","")
+  return rel_path(cleanpath), shortname
+end
+
+def create_index(path) # only called on layout = 3
   path = path + "/"
   f = File.open(path + "index.html","w")
   f << '<html><head><link rel="stylesheet" href="http://reganmian.net/alloy/build/aui-skin-classic/css/aui-skin-classic-all-min.css" type="text/css" media="screen" />
@@ -33,19 +45,15 @@ def create_index(path)
     if File.file?(node) then
       paths = node.split("/")
       name = paths[paths.size-1]
-      next if name == "index.html"
-      clean_name = sanitize_filename(name)
-      FileUtils.mv(path + name, path + clean_name) unless name == clean_name
-      shortname = name.gsub(".html","")
-      name.gsub!("'","\\'")
-      f << "<li><a href='#{clean_name}' target='content'>#{shortname}</a></li>"
+      next if skip?(name)
+      name, shortname = clean_name(node) # no path, in current directory
+      f << "<li><a href='#{File.basename(name)}' target='content'>#{shortname}</a></li>"
     end
   end
   f << "</ul></div></body></html>"
 end
 
 #######################################################################
-
 indent = 1
 
 `rm -rf #{OUT}`
@@ -72,14 +80,21 @@ Find.find(searchpath) do |path|
   end
   indent = cur_ind
   name = dirs[indent-1]
-
+  
+  filename = File.basename(path)
+  relpath = rel_path(path)
+#  relpath = "" if relpath = filename
+  
+  next if skip?(filename)
+  
   menufile << ("  " * indent) + "<li>" 
   if layout == 3
-    menufile << "<a href='#{path}/index.html' target='filelist'>#{name}</a>"
+    menufile << "<a href='#{relpath}/index.html' target='filelist'>#{name}</a>"
     create_index(path) 
   elsif layout == 2
     if File.file?(path)
-      menufile << "<a href='#{path}' target='content'>#{name}</a>"
+      name, shortname = clean_name(path)
+      menufile << "<a href='#{name}' target='content'>#{shortname}</a>"
     else
       menufile << "<a href='#'>#{name}</a>"
     end
