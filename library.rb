382dbe8dcd2067ef8a28b8e1ec9928b7e6ced95d
Stian Haklev <shaklev@gmail.com>
2011-02-16 10:53:34 -0500

    out dir, two layout htmls

---
+++ library.rb
@@ -0,0 +1,41 @@
+ALLOY = "file:///Users/stian/Downloads/alloy-1.0.1"
+
+def header(title)
+  return '<html>
+  <head>
+  <script src="'+ ALLOY + '/build/aui/aui.js" type="text/javascript"></script>
+
+  <link rel="stylesheet" href="'+ ALLOY + '/build/aui-skin-classic/css/aui-skin-classic-all-min.css" type="text/css" media="screen" />
+  </head>
+
+  <style type="text/css" media="screen">
+
+  </style>
+
+  <body>
+  <h1>' + title + '</h1>
+
+  <div id="markupBoundingBox">
+  <ul id="markupContentBox">'
+end
+
+def footer
+  return '</li></ul></div>
+
+  <script type="text/javascript" charset="utf-8">
+
+  AUI().ready("aui-tree-view", function(A) {
+
+    var treeView = new A.TreeView({
+      boundingBox: "#markupBoundingBox",
+      contentBox: "#markupContentBox"
+      })
+      .render();
+
+      });
+
+      </script>
+
+      </body>
+      </html>'
+end
