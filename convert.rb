3810a729fe1d755a9076107a8258dda40efec04f
Stian Haklev <shaklev@gmail.com>
2011-04-30 07:48:49 -0400

    added one note that contains all clippings, sorted

--- convert.rb
+++ convert.rb
@@ -1,9 +1,9 @@
 require 'pp'
 require 'rubygems'
 require 'appscript'
+include Appscript
 
 a = File.open("My Clippings.txt")
-include Appscript
 annotations = Hash.new
 
 while !a.eof?   # until we've gone through the whole file, line by line
@@ -28,10 +28,16 @@ while !a.eof?   # until we've gone through the whole file, line by line
   if loc.index("- Note") && annotations[title]
 
     if content == "t"
-      annotations[title][:title] += annotations[title][:clippings].pop[:text]
+      annotations[title][:title] += annotations[title][:clippings].pop[:text] + " "
       next
     elsif content == "a"
       annotations[title][:authors] += annotations[title][:clippings].pop[:text] + " "
+      next
+    elsif content == "r"
+      annotations[title][:references] += annotations[title][:clippings].pop[:text] + "<p>"
+      next
+    elsif content == "x"
+      annotations[title][:abstract] += annotations[title][:clippings].pop[:text] + "<p>"
       next
     elsif !content.index(" ")
       annotations[title][:clippings].last[:keyword] = content
@@ -39,57 +45,67 @@ while !a.eof?   # until we've gone through the whole file, line by line
     end
   end
 
-  
+
   label = loc.index("- Note") ? 2 : 0  # colors it blue if it is a note, rather than highlight
 
-  annotations[title] = {:clippings => Array.new, :title =>  "", :authors => "", :category => ""} unless annotations[title]
+  annotations[title] = {:clippings => Array.new, :title =>  "", :authors => "", :category => "", :abstract => "",
+    :references => ""} unless annotations[title]
 
-  if loc.index("- Note") && content[0..1] == "c "
-    annotations[title][:category] = content[2..-1] + "/"
-    next
+    if loc.index("- Note") && content[0..1] == "c "
+      annotations[title][:category] = content[2..-1] + "/"
+      next
+    end
+
+    loc = loc.gsub("- Note", "").gsub("Loc. ", "")
+    annotations[title][:clippings] << {:meta => meta, :text => content, :label => label, :loc => loc, :added => added}
   end
 
-  loc = loc.gsub("- Note", "").gsub("Loc. ", "")
-  annotations[title][:clippings] << {:meta => meta, :text => content, :label => label, :loc => loc, :added => added}
-end
+  # now let's connect to DevonThink, and create the notes
 
-pp annotations
-# now let's connect to DevonThink, and create the notes
+  dt = app('DevonThink Pro')
 
-dt = app('DevonThink Pro')
+  annotations.each do |title, book|
+    itemtitle = book[:title] == "" ? title[0..30] : book[:title][0..30]
+    folder = dt.create_location("Kindle/#{book[:category]}#{itemtitle}").get
 
-annotations.each do |title, book|
-  itemtitle = book[:title] == "" ? title[0..30] : book[:title][0..30]
-  folder = dt.create_location("Kindle/#{book[:category]}#{itemtitle}").get
+    # preload the clippings with info from abstract/references, and a scaffolded "own notes" note. these will be inserted
+    # just like any other clipping further below
+    if book[:references] != ""
+      book[:clippings] << {:meta => "", :text => "References" + " " * 40 + book[:references], :label => 3, :loc => 0, :added => Time.now}
+    end 
+    if book[:abstract] != ""
+      book[:clippings] << {:meta => "", :text => "Abstract" + " " * 40 + book[:abstract], :label => 4, :loc => 0, :added => Time.now}
+    end 
+    book[:clippings] << {:meta => "", :text => "Own notes" + " " * 40 , :label => 5, :loc => 0, :added => Time.now}
 
-  book[:clippings].each do |item|  
-    dt.create_record_with( { :name => item[:text][0...100], :type_ => :html, :rich_text => 
-      "<b>Title</b>: #{book[:title]}<br><b>Authors: </b>#{book[:authors]}</b><br><b>Loc:</b> #{item[:loc]}<p>#{item[:text]}", 
-      :tags => item[:keyword], :comment => item[:meta], :label => item[:label], :creation_date => item[:added] }, {:in => folder} )
-  end
-end
 
-  # annotations.each do |id, paper|
-  #   puts "Title: #{paper[:title]}"
-  #   puts "Authors: #{paper[:authors]}"
-  #   puts "*****"
-  #   
-  #   paper[:clippings].each do |clipping|
-  #     puts "Keyword: #{clipping[:keyword]}"
-  #     puts "Meta: #{clipping[:meta]}"
-  #     puts "\n#{clipping[:text]}"
-  #     puts "==================="
-  #   end
-  #   puts "*" * 50
-  # end
+    # insert all the clippings (included the ones preloaded above) into DevonThink. Thank you AppleScript!
+    book[:clippings].sort! {|y, x| y[:loc].to_i <=> x[:loc].to_i }
+    cliptext = "Title: #{book[:title]}\nAuthors: #{book[:authors]}"
+    book[:clippings].each do |item|  
 
-  # Kindle User's Guide (Amazon)
-  # - Highlight Loc. 881  | Added on Wednesday, January 05, 2011, 05:24 PM
-  # 
-  # dictionary
-  # ==========
-  # Introductions to ijCSCL (Gerry Stahl)
-  # - Highlight Loc. 493-95  | Added on Wednesday, January 05, 2011, 05:39 PM
-  # 
-  # While it may have been feasible to make progress on CSCL problems during the first decade of the field’s existence from exclusively within an educational psychology perspective or using an artificial intelligence approach, it is less likely now.
-  # ==========
+      dt.create_record_with( { :name => item[:text][0...40], :type_ => :text, :rich_text => "Title: #{book[:title]}\nAuthors: #{book[:authors]}\nLoc: #{item[:loc]}\n\n#{item[:text]}", :tags => item[:keyword], :comment => item[:meta], :label => item[:label], :creation_date => item[:added] }, {:in => folder} )
+      cliptext << "Loc: #{item[:loc]}\n\n#{item[:text]}\n----\n\n" 
+
+    end
+    dt.create_record_with( { :name => "All clippings", :type_ => :text, :rich_text => 
+      cliptext, :label => 3 }, {:in => folder} )
+
+    end
+
+
+    # for testing purposes: 
+
+    # annotations.each do |id, paper|
+    #   puts "Title: #{paper[:title]}"
+    #   puts "Authors: #{paper[:authors]}"
+    #   puts "*****"
+    #   
+    #   paper[:clippings].each do |clipping|
+    #     puts "Keyword: #{clipping[:keyword]}"
+    #     puts "Meta: #{clipping[:meta]}"
+    #     puts "\n#{clipping[:text]}"
+    #     puts "==================="
+    #   end
+    #   puts "*" * 50
+    # end
