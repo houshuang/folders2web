a283440c9c7c3eca138bfa7f9444253eb4b80c45
Stian Haklev <shaklev@gmail.com>
2011-05-22 15:14:23 +0800

    updating quicklook

---
+++ qlmanage-doc2pdf.rb
@@ -0,0 +1,29 @@
+# encoding: UTF-8
+# this script takes a doc file as input (or anything, really), and generates a PDF, using qlmanage and wkhtmltopdf
+
+require 'appscript'
+include Appscript
+require 'cgi'
+
+def do_pdf(path)
+  arg = path
+  puts path
+  file = File.basename(path)
+  `qlmanage -p -o /tmp "#{arg}"`
+  if File.exists?("/tmp/#{file}.qlpreview/Preview.html")
+    a = File.read("/tmp/#{file}.qlpreview/Preview.html") 
+    b = a.gsub('<div class="PageStyle">',"").gsub('{background:#ACB2BB;}','')
+    File.open("/tmp/qlmanage-doc2pdf-tmp.html","w") {|f| f << b}
+    `wkhtmltopdf /tmp/qlmanage-doc2pdf-tmp.html "#{arg}.pdf"`
+    app('Finder').reveal( MacTypes::FileURL.path(arg + ".pdf"))
+  else
+    `/usr/local/bin/growlnotify -t "Not able to convert to PDF" -m "Preview could not convert #{file}"`
+  end
+end
+
+app('Finder').selection.get.each do |item|
+  url =CGI::unescape(item.URL.get)
+  do_pdf(url[16..-1])
+end
+`/usr/local/bin/growlnotify -m "Conversion complete"`
+
