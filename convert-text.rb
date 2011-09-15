eb8df63b535fa32a1ae4602d97f28c8741323aca
Stian Håklev <shaklev@gmail.com>
2011-09-14 21:31:51 -0400

    handle braces and without braces

--- convert-text.rb
+++ convert-text.rb
@@ -6,11 +6,12 @@ require 'utility-functions'
 bib = json_bib
 citations = Hash.new
 doc = utf8safe(pbpaste)
-doc.scan( /\@([a-zA-Z]+[0-9]+[a-zA-Z]+)/).each do |hit|
+doc.scan( /(\[?\@[a-zA-Z]+[0-9]+[a-zA-Z]+\]?)/ ).each do |hit|
   hit = hit[0]
-  if bib[hit]
-    citations[hit] = bib[hit] 
-    doc.gsub!("@#{hit}", citations[hit][0] + ", " + citations[hit][1])
+  hitnobraces = hit.gsub(/[\@\[\]]/,"")
+  if bib[hitnobraces]
+    citations[hitnobraces] = bib[hitnobraces] 
+    doc.gsub!(hit, citations[hitnobraces][0] + ", " + citations[hitnobraces][1])
   end
 end
 doc << "\n\n\References\n"
