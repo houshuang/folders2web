627ee0e15b6147b427f21a8947fd940dbb6775e4
Stian Håklev <shaklev@gmail.com>
2011-09-26 11:33:00 -0400

    adding cmdline arg onlylist to convert-text, only bibliography, no in-text citations

--- convert-text.rb
+++ convert-text.rb
@@ -1,8 +1,12 @@
 # encoding: UTF-8
 $:.push(File.dirname($0))
-require 'pp'
 require 'utility-functions'
 
+# grabs currently selected text, and transforms [@scardamalia2006knowledge] citations to APA citations. with 
+# onlylist, it only generates a bibliography, without it it creates in-text citations (Scardamalia, 2006) and
+# a bibliography
+
+onlylist = (ARGV[0] == 'onlylist')
 bib = json_bib
 citations = Hash.new
 doc = utf8safe(pbpaste)
@@ -11,12 +15,16 @@ doc.scan( /(\[?\@[a-zA-Z]+[0-9]+[a-zA-Z]+\]?)/ ).each do |hit|
   hitnobraces = hit.gsub(/[\@\[\]]/,"")
   if bib[hitnobraces]
     citations[hitnobraces] = bib[hitnobraces] 
-    doc.gsub!(hit, citations[hitnobraces][0] + ", " + citations[hitnobraces][1])
+    if onlylist
+      doc.gsub!(hit, "")
+    else
+      doc.gsub!(hit, citations[hitnobraces][0] + ", " + citations[hitnobraces][1])
+    end
   end
 end
-doc << "\n\n\References\n"
+doc << "\n\n\References\n" unless onlylist
 citations.sort.each do |item|
-  doc << item[1][2] + "\n"
+  doc << item[1][2].gsub(/[\{\}]/,'') + "\n"
 end
 
-puts doc
+puts doc.strip
