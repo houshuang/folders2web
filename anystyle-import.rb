c7c5eba9fd5dd8bab9cefd1cc9ef79a84feb6e28
Stian Håklev <shaklev@gmail.com>
2012-02-21 12:30:59 -0500

    enabling automatic import of publications based on bibtex through anystyle-import script

--- anystyle-import.rb
+++ anystyle-import.rb
@@ -1,6 +1,15 @@
+# encoding: UTF-8
 $:.push(File.dirname($0))
 require 'utility-functions'
 require 'appscript'
 require 'anystyle/parser'
 
-pbcopy (Anystyle.parse(pbpaste, :bibtex).to_s)
+search = utf8safe(pbpaste)
+if search.index("doi:")
+  require 'doi-lookup/doi-bibtex.rb'
+  bibtex = lookup_doi(search, Crossref_API)
+  growl "Failure", "DOI lookup not successful" unless bibtex
+else
+  bibtex = Anystyle.parse(search, :bibtex).to_s
+end
+pbcopy (bibtex)
