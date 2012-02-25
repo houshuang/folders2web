44ffa2eb31480f0492ff07fd6aef92729de23194
Stian Håklev <shaklev@gmail.com>
2012-02-24 20:09:44 -0500

    Major improvements, actually works pretty well now (bibtex-cleanup)

--- bibtex-cleanup.rb
+++ bibtex-cleanup.rb
@@ -1,31 +1,53 @@
 # encoding: UTF-8
+$:.push(File.dirname($0))
+require 'utility-functions'
 
 require 'bibtex'
 require 'namecase'
 require 'unicode'
 require 'latex/decode'
 require 'prettyprint'
-
-cit = '@inproceedings{keselj4ppdn,
-	Author = {Ke{\v{s}}elj, V. and Cercone, N. and MACDOUGAL, K.D.A.},
-	Date-Added = {2012-02-19 23:37:27 +0000},
-	Date-Modified = {2012-02-19 23:37:35 +0000},
-	Keywords = {keyword1, keyword2, keyword3, keyword4},
-	Organization = {Citeseer},
-	Title = {PPDN—A Framework f{\aa}r Peer-to-peer Collaborative Research Network},
-}'
+require 'iconv'
 
 
 class Fix_namecase < BibTeX::Filter
 	def apply(field)
 		temp = NameCase(Unicode::capitalize(field))
-	
+
 		# fix problem of initials without space between
 		temp.to_s.gsub(/\.([A-Za-z])/, '. \1')
 	end
 end
 
-b = BibTeX::parse(cit, :filter => :latex)
-b[0][:author].convert!(:fix_namecase)
+module BibTeX
+	class Entry
+		def std_key
+			k = names[0]
+			k = k.respond_to?(:family) ? k.family : k.to_s
+			cstr = Iconv.conv('us-ascii//translit', 'utf-8', k)
+			cstr << (has_field?(:year) ? year : '')
+			t = title.dup.split.select {|f| f.size > 3}[0]
+			cstr << t ? t : ''
+			cstr.downcase.gsub!(/[^a-zA-Z0-9\-]/, '')
+		end
+	end
+end
 
-puts b.to_s
+def cleanup_bibtex(cit)
+	b = BibTeX::parse(cit, :filter => :latex)
+	b[0][:author].convert!(:fix_namecase)
+	b.each do |e| 
+		e.key = e.std_key 
+	end
+	b.to_s
+end
+
+# cit= '@article{cakir2002effectiveness,title={Effectiveness of conceptual change text-oriented instruction on students\' understanding of cellular respiration concepts},author={{\c{C}}akir, {\"O}.S. and Geban, {\"O}. and Y{\"u}r{\"u}k, N.},journal={Biochemistry and Molecular Biology Education},volume={30},number={4},pages={239--243},year={2002},publisher={Wiley Online Library}},keywords={keyword1, keyword2},'
+
+cit = pbpaste
+
+# first fix key, because bibtex chokes on unicode keys
+cit.gsub!(/\@(.+?)\{(.+?)\,(.+?)$/m, '@\1{key,\3')
+
+newcit = cleanup_bibtex(cit)
+pbcopy(cleanup_bibtex(cit))
