3186d7012ff4903a7006f70b3cd6f5900afb823b
Stian Håklev <shaklev@gmail.com>
2012-02-24 19:58:52 -0500

    changed pbcopy and pbpaste to use AppleScript, for better UTF8 support

--- utility-functions.rb
+++ utility-functions.rb
@@ -71,13 +71,14 @@ end
 
 
 # writes text to clipboard, using a pipe to avoid shell mangling
+# rewritten using osascript for better UTF8 support (from http://www.coderaptors.com/?action=browse&diff=1&id=Random_tips_for_Mac_OS_X)
 def pbcopy(text)
-  IO.popen("pbcopy","w+") {|pipe| pipe << text}
+  IO.popen("osascript -e 'set the clipboard to do shell script \"cat\"'","w+") {|pipe| pipe << text}
 end
 
-
+# gets text from clipboard
 def pbpaste
-  IO.popen('pbpaste', 'r+').read
+  IO.popen("osascript -e 'the clipboard as unicode text' | tr '\r' '\n'", 'r+').read
 end
 
 
