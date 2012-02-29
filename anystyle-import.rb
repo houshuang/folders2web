8691af3427100269383264b2e60447c6f85366b2
Stian Håklev <shaklev@gmail.com>
2012-02-29 02:06:11 -0500

    Change anystyle-import to use doi content negotiation.

--- anystyle-import.rb
+++ anystyle-import.rb
@@ -1,15 +1,23 @@
 # encoding: UTF-8
 $:.push(File.dirname($0))
+require 'open-uri'
 require 'utility-functions'
-require 'appscript'
-require 'anystyle/parser'
 
-search = utf8safe(pbpaste)
-if search.index("doi:")
-  require 'doi-lookup/doi-bibtex.rb'
+# grab from clipboard, either look up DOI through API, or 
+# use anystyle parser to convert text to bibtex. Paste to clipboard.
+
+def lookup_doi(doi, crossref_api_key)
+	doi = doi.downcase.gsub('doi:','').gsub('http://','').gsub('dx.doi.org/','').gsub('doi>','').gsub('doi ','').strip
+  url = "http://dx.doi.org/#{doi}"
+	return open(url, "Accept" => "text/bibliography; style=bibtex").read
+end
+
+search = pbpaste
+if search.strip[0..2] == "doi"
   bibtex = lookup_doi(search, Crossref_API)
   growl "Failure", "DOI lookup not successful" unless bibtex
 else
+  require 'anystyle/parser'
   bibtex = Anystyle.parse(search, :bibtex).to_s
 end
 pbcopy (bibtex)
