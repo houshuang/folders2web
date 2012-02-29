0b375325f2497358e956635c714aa9abe81ca516
Stian Håklev <shaklev@gmail.com>
2012-02-29 12:15:26 -0500

    getting bibtex from bibdesk instead of bibliography.bib in wiki-lib

--- wiki-lib.rb
+++ wiki-lib.rb
@@ -23,12 +23,13 @@ def ensure_refpage(citekey,override=false)
       growl("File not found", "Cannot find citation #{citekey} in BibDesk, the citekey in BibDesk needs to be the same as the name of the PDF.")
       exit
     end
-    bib = BibTeX.parse(find[0].BibTeX_string.get.to_s)
-    bib.parse_names
-    item = bib[citekey.to_sym]
-    p item
+
+    bib = find[0].BibTeX_string.get
+
+    bib = BibTeX.parse(bib)
+    item = bib[0]
+
     citation = CiteProc.process(item.to_citeproc, :style => :apa)
-    p item.class
     javascript = "javascript:var MyFrame='<frameset cols=\\'*,*\\'><frame src=\\'/wiki/notes:#{citekey}?do=edit&vecdo=print\\'><frame src=\\'/wiki/clip:#{citekey}?vecdo=print\\'></frameset>';with(document) {    write(MyFrame);};return false;\""
     citation = "^ Citation |#{cb(citation)}  ^ <html><a href=\"#{javascript}\">Sidewiki</a></html>^\n^[[bibdeskx://#{citekey}|BibDesk]] | ::: ^  [[skimx://#{citekey}|PDF]] ^ "
     text = "h1. #{cb(item[:title])}\n\n#{citation}\n\n<hidden BibTex>\n  #{item.to_s}\n</hidden>\n\n{{page>notes:#{citekey}}}\n\nh2. Links here\n{{backlinks>.}}\n\n{{page>clip:#{citekey}}}\n\n{{page>kindle:#{citekey}}}\n\n{{page>skimg:#{citekey}}}"
@@ -39,13 +40,13 @@ end
 def make_newimports_page(ary)
   # b = BibTeX.open("/Volumes/Home/stian/Dropbox/Archive/Bibliography.bib")
   # b.parse_names
-  # 
+  #
   # out = "h1. Recently imported items\n\n^Note name ^ Note text ^\n"
   # ary.each do |citekey|
   #   item = b[citekey.to_sym]
   #   cit = CiteProc.process item.to_citeproc, :style => :apa
   #   out << "| [[:ref:#{item.key}]] | #{cit}|\n"
   # end
-  # 
-  # File.open('/wiki/data/pages/bib/recent_imports.txt', 'w') {|f| f << out}  
+  #
+  # File.open('/wiki/data/pages/bib/recent_imports.txt', 'w') {|f| f << out}
 end
