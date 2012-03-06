fbc9a634726f19f236ea63d1d6c492115f86b627
Stian Håklev <shaklev@gmail.com>
2012-03-05 23:45:52 -0500

    Don't need crossref api

--- anystyle-import.rb
+++ anystyle-import.rb
@@ -3,10 +3,10 @@ $:.push(File.dirname($0))
 require 'open-uri'
 require 'utility-functions'
 
-# grab from clipboard, either look up DOI through API, or 
+# grab from clipboard, either look up DOI through API, or
 # use anystyle parser to convert text to bibtex. Paste to clipboard.
 
-def lookup_doi(doi, crossref_api_key)
+def lookup_doi(doi)
 	doi = doi.downcase.gsub('doi:','').gsub('http://','').gsub('dx.doi.org/','').gsub('doi>','').gsub('doi ','').strip
   url = "http://dx.doi.org/#{doi}"
 	return open(url, "Accept" => "text/bibliography; style=bibtex").read
@@ -14,7 +14,7 @@ end
 
 search = pbpaste
 if search.strip[0..2] == "doi"
-  bibtex = lookup_doi(search, Crossref_API)
+  bibtex = lookup_doi(search)
   growl "Failure", "DOI lookup not successful" unless bibtex
 else
   require 'anystyle/parser'
