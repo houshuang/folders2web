56183af0aa894d0bfd976390a1a1e350f9581aac
Stian Håklev <shaklev@gmail.com>
2012-03-20 15:56:35 +0100

    Script to convert from scrivener to dokuwiki

---
+++ convert-markdown-dokuwiki.rb
@@ -0,0 +1,23 @@
+# encoding: UTF-8
+
+# remember to compile with Transformations...Convert Plain Text to Paragraph Spacing
+# small script to convert my style of writing markdown(ish) in Scrivener to dokuwiki
+
+$:.push(File.dirname($0))
+require 'utility-functions'
+
+a = File.read(ARGV[0])
+
+# convert to paragraph spacing, if not already the case
+a.gsub!(/([^\n])\n([^\n])/m,'\1' + "\n\n" + '\2')
+
+# convert bullet lists with - and tabs to * and spaces
+a.gsub!(/^(\t*)- /) { |f| "  " + f.gsubs([/\t/, '  '], ["- ", "* "]) }
+
+# no double-spacing between bullet items
+a.gsub!(/\*(.+?)\n\n[^ ]/m) {|f| f.gsub("\n\n", "\n")[0..-2] + "\n" + f[-1] }
+
+# clean up spaces before titles
+a.gsub!(/^ +(h[1-9]\.)/) {|f| p f}#, '\1')
+
+File.write(ARGV[0], a)
