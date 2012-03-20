180ef1be027e9f2fc3527b0caf9364cc7053a841
Stian Håklev <shaklev@gmail.com>
2012-03-20 11:37:04 -0400

    Cleaning up convert-markdown with gsubs!

--- convert-markdown-dokuwiki.rb
+++ convert-markdown-dokuwiki.rb
@@ -8,19 +8,17 @@ require 'utility-functions'
 
 a = File.read(ARGV[0])
 
-# convert to paragraph spacing, if not already the case (one blank line between each paragraph)
-a.gsub!(/([^\n])\n([^\n])/m, '\1' + "\n\n" + '\2')
+a.gsubs!(
+  [/([^\n])\n([^\n])/m, '\1' + "\n\n" + '\2'],    # convert to paragraph spacing
+  [/.(h[1-9]\.)/, '\1'],                          # clean up spaces before titles
+  [/\n\n\n+/, "\n\n"],                            # remove extraneous linespacing
+  [/^b.(.+?)$/, '<blockquote>\1</blockquote>']
+  )
 
 # convert bullet lists with - and tabs to * and spaces
 a.gsub!(/^(\t*)- /) { |f| "  " + f.gsubs([/\t/, '  '], ["- ", "* "]) }
 
 # no double-spacing between bullet items
 a.gsub!(/\*(.+?)\n\n[^ ]/m) {|f| f.gsub("\n\n", "\n")[0..-2] + "\n" + f[-1] }
-
-# clean up spaces before titles
-a.gsub!(/.(h[1-9]\.)/, '\1') # {|f| p f}
-
-# remove extraneous linespacing
-a.gsub!(/\n\n\n+/, "\n\n")
 
 File.write(ARGV[0]+".txt", a)
