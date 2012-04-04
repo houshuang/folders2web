61568ad4da82f3faf05d98456678e5ee3d5333ca
Stian Håklev <shaklev@gmail.com>
2012-04-04 17:03:55 -0400

    Speed-up and improved formatting
    I only generate a citation once, and store it in item[:cit], reused a number of times.
    Also only list publications which have notes or clippings on main bibliography - all are still listed on author/kw etc.
    Names of publications/authors/keywords are no longer printed, but individual and total times are.
    Changed bibliography to use Dokuwiki tables instead of HTML (makes links work better local and remote).
    Some reformatting for legibility.

--- bibtex-batch.rb
+++ bibtex-batch.rb
@@ -56,13 +56,19 @@ def make_rss_feed
   File.write(Wiki_path + "/feed.xml", feedcontent)
 end
 
+timetot = Time.now
+timetmp = Time.now
+
 puts "Making RSS feed"
+
 make_rss_feed
-puts "RSS feed complete (will be updated next time you sync with server)"
+puts "RSS feed complete (will be updated next time you sync with server) (#{Time.now - timetmp} s.)"
+timetmp = Time.now
 puts "Parsing BibTeX"
 b = BibTeX.parse(File.read(Bibliography))
 b.parse_names
-puts "Initial parse complete"
+puts "Initial parse complete (#{Time.now - timetmp} s.)"
+timetmp = Time.now
 out1 = ''
 out2 = ''
 out3 = ''
@@ -78,9 +84,9 @@ counter[:noref] = 0
 counter[:notes] = 0
 counter[:clippings] = 0
 counter[:images] = 0
+
 puts "Starting secondary parse"
 b.each do |item|
-  puts item.key
   ax = []
   if item.respond_to? :author
     item.author.each do |a|
@@ -103,13 +109,17 @@ b.each do |item|
   end
 
   cit = CiteProc.process item.to_citeproc, :style => :apa
+  cit.gsub!(/Retrieved from(.+?)$/, '')
+  item[:cit] = cit
   year = (defined? item.year) ? item.year.to_s : "n.d."
   if year == "n.d." and cit.match(/\((....)\)/)
     year = $1
   end
   json[item.key.to_s] = [namify(ax), year, cit]
+
   hasfiles = Array.new
-  hasfiles[4]=""
+  hasfiles[2] = '' # ensure that array is filled even if some fields are empty, for alignment
+
   if File.exists?("#{Wiki_path}/data/pages/ref/#{item.key}.txt")
     counter[:hasref] += 1
     if File.exists?("#{Wiki_path}/data/pages/clip/#{item.key}.txt") || File.exists?("#{Wiki_path}/data/pages/kindle/#{item.key}.txt")
@@ -123,18 +133,15 @@ b.each do |item|
     if File.exists?("#{Wiki_path}/data/pages/notes/#{item.key}.txt")
       counter[:notes] += 1
       hasfiles[0] = "N"
-      out1 << "<tr><td><a href = 'ref:#{item.key}'>#{item.key}</a></td><td>#{hasfiles.join("</td><td>&nbsp;")}</td><td>#{cit}</td></tr>\n"
-    elsif hasfiles[1] == "C"
-      out2 << "<tr><td><a href = 'ref:#{item.key}'>#{item.key}</a></td><td>#{hasfiles.join("</td><td>&nbsp;")}</td><td>#{cit}</td></tr>\n"
-    else
-      out3 << "<tr><td><a href = 'ref:#{item.key}'>#{item.key}</a></td><td>#{hasfiles.join("</td><td>&nbsp;")}</td><td>#{cit}</td></tr>\n"
-
-
     end
 
-  else
-    counter[:noref] += 1
-    out4 << "<tr><td>#{item.key}</td><td>#{hasfiles.join("</td><td>&nbsp;")}</td><td>#{cit}</td></tr>\n"
+    txt = "| [#:ref:#{item.key}|#{item.key}] | #{hasfiles.join(" | ")} |#{cit}|\n"
+
+    if hasfiles[0] == "N"
+      out1 << txt
+    elsif hasfiles[1] == "C"
+      out2 << txt
+    end
   end
 
   # mark as read if notes exist
@@ -144,16 +151,22 @@ b.each do |item|
 
 end
 
+puts "Finished secondary parse, generating main bibliography (#{Time.now - timetmp} s.)"
+timetmp = Time.now
 
-puts "Finished secondary parse, generating main bibliography"
-
-out = "h1. Bibliography\n\nDownload [[http://dl.dropbox.com/u/1341682/Bibliography.bib|entire BibTeX file]]. Also see bibliography by [[abib:start|author]] or by [[kbib:start|keyword]].\n\nPublications that have their own pages are listed on top, and hyperlinked. Most of these also have clippings and many have key ideas.\n\nStatistics: Totally **#{counter[:hasref] + counter[:noref]}** publications, and **#{counter[:hasref]}** publications have their own wikipages. Of these, **#{counter[:images]}** with notes (key ideas) **(N)**, **#{counter[:clippings]}** with highlights (imported from Kindle or Skim) **(C)**, and **#{counter[:images]}** with images (imported from Skim) **(I)** and.<html><table>"
+out = "h1. Bibliography\n\nDownload [[http://dl.dropbox.com/u/1341682/Bibliography.bib|entire BibTeX file]].
+Also see bibliography by [[abib:start|author]] or by [[kbib:start|keyword]].\n\nPublications that have their
+own pages are listed on top, and hyperlinked. Most of these also have clippings and many have key ideas.\n\n
+Statistics: Totally **#{counter[:hasref] + counter[:noref]}** publications, and **#{counter[:hasref]}**
+publications have their own wikipages. Of these, **#{counter[:images]}** with notes (key ideas) **(N)**,
+**#{counter[:clippings]}** with highlights (imported from Kindle or Skim) **(C)**, and **#{counter[:images]}**
+with images (imported from Skim) **(I)**\n\n"
 
 #dt.document.save
 
 File.open("#{Wiki_path}/lib/plugins/dokuresearchr/json.tmp","w"){|f| f << JSON.fast_generate(json)}
 
-out << out1 << out2 << out3 << out4 << "</table></html>"
+out << out1 << out2 << out3
 File.open("#{Wiki_path}/data/pages/bib/bibliography.txt", 'w') {|f| f << out}
 
 ###############################################
@@ -174,11 +187,10 @@ authors.each do |axx, pubs|
   out = "h2. #{author}'s publications\n\n"
   sort_pubs(pubs).each do |i|
     item = b[i]
-    cit = CiteProc.process item.to_citeproc, :style => :apa
     if File.exists?("#{Wiki_path}/data/pages/ref/#{item.key}.txt")
-      out1 << "| [#:ref:#{item.key}] | #{cit}|#{pdfpath(item.key)}|\n"
+      out1 << "| [#:ref:#{item.key}|#{item.key}] | #{item[:cit]}|#{pdfpath(item.key)}|\n"
     else
-      out2 << "| #{item.key} | #{cit}|#{pdfpath(item.key)}|\n"
+      out2 << "| #{item.key} | #{item[:cit]}|#{pdfpath(item.key)}|\n"
     end
   end
 
@@ -186,7 +198,6 @@ authors.each do |axx, pubs|
   authorname = clean_pagename(author)
   authorlisted << [authorname,author,pubs.size]
   File.open("#{Wiki_path}/data/pages/abib/#{authorname}.txt", 'w') {|f| f << out}
-  puts author
 end
 
 File.open("#{Wiki_path}/data/pages/abib/start.txt","w") do |f|
@@ -202,44 +213,46 @@ end
 end
 ###############################################
 # generate individual files for each keyword
-
+puts "Finished ((#{Time.now - timetmp} s.)"
 if keywordopt
+  timetmp = Time.now
   puts "Generating individual files for each keyword"
 
-keywordslisted = Array.new
-keywords.each do |keyword, pubs|
-  out =''
-  out1 = ''
-  out2 =''
-  out = "h2. Publications with keyword \"#{keyword}\"\n\n"
-  sort_pubs(pubs).each do |i|
-    item = b[i]
-    cit = CiteProc.process item.to_citeproc, :style => :apa
-    if File.exists?("#{Wiki_path}/data/pages/ref/#{item.key}.txt")
-      out1 << "| [#:ref:#{item.key}] | #{cit}| #{pdfpath(item.key)} |\n"
-    else
-      out2 << "| #{item.key} | #{cit} | #{pdfpath(item.key)}|\n"
+  keywordslisted = Array.new
+  keywords.each do |keyword, pubs|
+    out =''
+    out1 = ''
+    out2 =''
+    out = "h2. Publications with keyword \"#{keyword}\"\n\n"
+    sort_pubs(pubs).each do |i|
+      item = b[i]
+      if File.exists?("#{Wiki_path}/data/pages/ref/#{item.key}.txt")
+        out1 << "| [#:ref:#{item.key}|#{item.key}] | #{item[:cit]}| #{pdfpath(item.key)} |\n"
+      else
+        out2 << "| #{item.key} | #{item[:cit]} | #{pdfpath(item.key)}|\n"
+      end
+    end
+
+    out << out1 << out2
+    kwname = keyword.gsub(/[\,\.\/ ]/,"_").downcase
+    keywordslisted << [kwname,keyword,pubs.size]
+    File.open("#{Wiki_path}/data/pages/kbib/#{kwname}.txt", 'w') {|f| f << out}
+  end
+
+  File.open("#{Wiki_path}/data/pages/kbib/start.txt","w") do |f|
+    f << "h1. List of publication keywords\n\n"
+    keywordslisted.sort {|x,y| y[2].to_i <=> x[2].to_i}.each do |ax|
+      f << "|[##{ax[0]}|#{ax[1]}]|#{ax[2]}|\n"
     end
   end
+  puts "Finished ((#{Time.now - timetmp} s.)"
 
-  out << out1 << out2
-  kwname = keyword.gsub(/[\,\.\/ ]/,"_").downcase
-  keywordslisted << [kwname,keyword,pubs.size]
-  File.open("#{Wiki_path}/data/pages/kbib/#{kwname}.txt", 'w') {|f| f << out}
-  puts kwname
-end
-
-File.open("#{Wiki_path}/data/pages/kbib/start.txt","w") do |f|
-  f << "h1. List of publication keywords\n\n"
-  keywordslisted.sort {|x,y| y[2].to_i <=> x[2].to_i}.each do |ax|
-    f << "|[##{ax[0]}|#{ax[1]}]|#{ax[2]}|\n"
-  end
-end
 end
 ###############################################
 # generate individual files for each journal with more than five cits.
 
 if journalopt
+  timetmp = Time.now
   puts "Generating individual files for each journal"
 
 authorlisted = Array.new
@@ -248,18 +261,16 @@ journals.each do |axx, pubs|
   out1 = ''
   out2 =''
   author = axx.strip
-  p pubs, pubs.size
   next unless pubs.size > 5
   # only generates individual author pages for authors with full names. this is because I want to deduplicate author names
   # when you import bibtex, you get many different spellings etc.
   out = "h2. Publications in #{author}\n\n"
   sort_pubs(pubs).each do |i|
     item = b[i]
-    cit = CiteProc.process item.to_citeproc, :style => :apa
     if File.exists?("#{Wiki_path}/data/pages/ref/#{item.key}.txt")
-      out1 << "| [#:ref:#{item.key}] | #{cit}|#{pdfpath(item.key)}|\n"
+      out1 << "| [#:ref:#{item.key}|#{item.key}] | #{item[:cit]}|#{pdfpath(item.key)}|\n"
     else
-      out2 << "| #{item.key} | #{cit}|#{pdfpath(item.key)}|\n"
+      out2 << "| #{item.key} | #{item[:cit]}|#{pdfpath(item.key)}|\n"
     end
   end
 
@@ -267,7 +278,6 @@ journals.each do |axx, pubs|
   authorname = clean_pagename(author)
   authorlisted << [authorname,author,pubs.size]
   File.open("#{Wiki_path}/data/pages/jbib/#{authorname}.txt", 'w') {|f| f << out}
-  puts author
 end
 end
 
@@ -280,6 +290,7 @@ File.open("#{Wiki_path}/data/pages/jbib/start.txt","w") do |f|
     end
     f << "| [##{ax[0]}|#{ax[1]}] | #{apage} |#{ax[2]}|\n"
   end
+  puts "Finished ((#{Time.now - timetmp} s.)"
 end
 
 
@@ -297,3 +308,5 @@ end
 #   end
 # end
 # File.open("#{Wiki_path}/data/pages/bib/needs_key_ideas.txt","w") {|f| f << out}
+
+puts "All tasks finished. Total time #{Time.now - timetot} s."
