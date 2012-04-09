0e323f44db069a93321103abc16732b3d4c5798b
Stian Håklev <shaklev@gmail.com>
2012-04-08 21:35:10 -0400

    Various improvements to cleanup - show last 7 days

--- cleanup.rb
+++ cleanup.rb
@@ -4,9 +4,11 @@
 
 $:.push(File.dirname($0))
 require 'utility-functions'
+require 'appscript'
+include Appscript
 
 def bname(ary)
-  ary.map {|f| File.basename(f)}
+  ary.map {|f| File.basename(f).remove(".txt")}
 end
 
 refs = bname(Dir[Wiki_path + "/data/pages/ref/*.txt"])
@@ -15,25 +17,45 @@ clips = bname(Dir[Wiki_path + "/data/pages/clip/*.txt"])
 notes = bname(Dir[Wiki_path + "/data/pages/notes/*.txt"])
 kindle = bname(Dir[Wiki_path + "/data/pages/kindle/*.txt"])
 
+refs_week = bname(`find /wiki/data/pages/ref/*.txt -mtime -7`.split("\n"))
+notes_week = bname(`find /wiki/data/pages/notes/*.txt -mtime -7`.split("\n"))
+
+notes_short_week = []
+notes_week.each {|f| notes_short_week << f unless File.size("#{Wiki_path}/data/pages/notes/#{f}.txt") > 200}
+
+# check off all the notes in BibDesk, takes a few seconds
+# notes.each do |n|
+#   bibdesk_publication = try { app("BibDesk").document.search({:for =>n})[0] }
+#   bibdesk_publication.fields["Notes"].value.set("1") if bibdesk_publication
+# end
+
 puts "<html><head>Researchr cleanup script report</title></head>"
 puts "<h1>Researchr cleanup script report</h1>"
 
-puts "<h2>Notes pages without ref page</h2>"
-(notes - refs).each {|a| puts "<li><a href='#{Internet_path}/notes:#{a.remove(".txt")}'>#{a.remove(".txt")}</a></li>"}
+puts "<h2>New publications added last 7 days with decent-sized notes</h2>"
+(notes_week - notes_short_week).each {|a| puts "<li><a href='#{Internet_path}/ref:#{a}'>#{a}</a></li>"}
+
+puts "<h2>New publications added last 7 days with brief</h2>"
+(notes_short_week).each {|a| puts "<li><a href='#{Internet_path}/ref:#{a}'>#{a}</a></li>"}
+
+puts "<h2>New publications added last 7 days without notes</h2>"
+(refs_week - notes_week).each {|a| puts "<li><a href='#{Internet_path}/ref:#{a}'>#{a}</a></li>"}
+
+puts "<hr><h2>Notes pages without ref page</h2>"
+(notes - refs).each {|a| puts "<li><a href='#{Internet_path}/notes:#{a}'>#{a}</a></li>"}
 
 puts "<h2>Clipping pages without ref page</h2>"
-(clips - refs).each {|a| puts "<li><a href='#{Internet_path}/clip:#{a.remove(".txt")}'>#{a.remove(".txt")}</a></li>"}
+(clips - refs).each {|a| puts "<li><a href='#{Internet_path}/clip:#{a}'>#{a}</a></li>"}
 
 puts "<h2>Image pages without ref page</h2>"
-(skimg - refs).each {|a| puts "<li><a href='#{Internet_path}/skimg:#{a.remove(".txt")}'>#{a.remove(".txt")}</a></li>"}
+(skimg - refs).each {|a| puts "<li><a href='#{Internet_path}/skimg:#{a}'>#{a}</a></li>"}
 
 puts "<h2>Kindle pages without ref page</h2>"
-(kindle - refs).each {|a| puts "<li><a href='#{Internet_path}/kindle:#{a.remove(".txt")}'>#{a.remove(".txt")}</a></li>"}
+(kindle - refs).each {|a| puts "<li><a href='#{Internet_path}/kindle:#{a}'>#{a}</a></li>"}
 
 puts "<h2>Ref pages with no sub-pages</h2>"
-(refs - (skimg + clips + notes + kindle)).each {|a| puts "<li><a href='#{Internet_path}/ref:#{a.remove(".txt")}'>#{a.remove(".txt")}</a></li>"}
+(refs - (skimg + clips + notes + kindle)).each {|a| puts "<li><a href='#{Internet_path}/ref:#{a}'>#{a}</a></li>"}
 
-
-exit(0)
-puts "<h2>Kindle pages that also has clipping page</h2>"
-().each {|a| puts "<li><a href='#{Internet_path}/kindle:#{a.remove(".txt")}'>#{a.remove(".txt")}</a></li>"}
+# how to do a union of two arrays?
+#puts "<h2>Kindle pages that also has clipping page</h2>"
+#(kindle ).each {|a| puts "<li><a href='#{Internet_path}/kindle:#{a}'>#{a}</a></li>"}
