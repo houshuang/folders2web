a2da690794054936b467853154bd4fa49dd538a7
Stian Håklev <shaklev@gmail.com>
2012-04-08 22:34:45 -0400

    Added numbers of publications for this week's stats in cleanup

--- cleanup.rb
+++ cleanup.rb
@@ -32,14 +32,16 @@ notes_week.each {|f| notes_short_week << f unless File.size("#{Wiki_path}/data/p
 puts "<html><head><title>Researchr cleanup script report</title></head><body>"
 puts "<h1>Researchr cleanup script report</h1>"
 
-puts "<h2>New publications added last 7 days with decent-sized notes</h2>"
-(notes_week - notes_short_week).each {|a| puts "<li><a href='#{Internet_path}/ref:#{a}'>#{a}</a></li>"}
+this = notes_week - notes_short_week
+puts "<h2>New publications added last 7 days with decent-sized notes (#{this.size})</h2>"
+this.each {|a| puts "<li><a href='#{Internet_path}/ref:#{a}'>#{a}</a></li>"}
 
-puts "<h2>New publications added last 7 days with brief notes</h2>"
+puts "<h2>New publications added last 7 days with brief notes (#{notes_short_week.size})</h2>"
 (notes_short_week).each {|a| puts "<li><a href='#{Internet_path}/ref:#{a}'>#{a}</a></li>"}
 
-puts "<h2>New publications added last 7 days without notes</h2>"
-(refs_week - notes_week).each {|a| puts "<li><a href='#{Internet_path}/ref:#{a}'>#{a}</a></li>"}
+this = refs_week - notes_week
+puts "<h2>New publications added last 7 days without notes (#{this.size})</h2>"
+this.each {|a| puts "<li><a href='#{Internet_path}/ref:#{a}'>#{a}</a></li>"}
 
 puts "<hr><h2>Notes pages without ref page</h2>"
 (notes - refs).each {|a| puts "<li><a href='#{Internet_path}/notes:#{a}'>#{a}</a></li>"}
