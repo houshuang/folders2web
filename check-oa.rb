08f513e30c384487ee11f111aa866b911f231ff5
Stian Håklev <shaklev@gmail.com>
2012-04-17 10:19:06 -0700

    cleaning up check-oa

--- check-oa.rb
+++ check-oa.rb
@@ -2,12 +2,11 @@
 require 'net/http'
 require "cgi"
 
-
 # this runs on my server, and is used to check if a certain url links to a publicly available PDF
 
 def checkOA(url)
-  url = url.remove(/http:\/\/?/)
-  uri,*path = url.split("/")
+  url = url.gsub("http:/",'').gsub("http://",'')
+  uri, *path = url.split("/")
   path = "/" + path.join("/")
   response = nil
   Net::HTTP.start(uri, 80) { |http| response = http.head(path) }
@@ -20,14 +19,6 @@ def checkOA(url)
   return possible_ctypes.index( response['content-type'] )
 end
 
-cgi = CGI.new
-arg = cgi['redir'].to_s
-
+arg = CGI.new['redir'].to_s
 puts "Content-Type: text/html; charset=utf-8\n\n"
-#puts "<html><head><meta http-equiv=\"content-type\" content=\"text-html; charset=utf-8\">"
-
-if checkOA(arg)
-  puts "true"
-else
-  puts "false"
-end
+puts checkOA(arg) ? 'true' : 'false'
