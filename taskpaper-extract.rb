527c585a4db9a5b2f9f055c8f05b59f03d52edba
Stian Håklev <shaklev@gmail.com>
2012-04-18 17:05:09 -0400

    Fixed it - seems to work nicely. Could make it easier to configure directories, output format etc.

--- taskpaper-extract.rb
+++ taskpaper-extract.rb
@@ -1,71 +1,73 @@
 # encoding: UTF-8
 $:.push(File.dirname($0))
 require 'utility-functions'
-require 'pp'
+require 'ap'
 
-#a = File.read('/Users/Stian/Dropbox/PhD/Litreview April 2012.taskpaper')
+a = File.read('/Users/Stian/Dropbox/PhD/Litreview April 2012.taskpaper')
 
-a="
-Paragogy:
-[@corneli2012detecting] math learning online @online
-\tlearning model @tomato
-\t\tA naıve idea: model learning as vocabulary acquisition @p2pu
-\t\tMore complete: model learning as a change in patterns of behavior
-\t\tSophisticated: model learning in terms of the use of new heuristic strategies
-\tevaluate learning - look for @peter
-\t\tWorking at the cutting edge (introducing new material?) @onlyone
-\t\tProgressive problem solving (working at increasing depth?)
-\t\tCollaborative effort (asking or answering questions?)
-\t\tUser-Identified high points (bookmarked threads and articles?)
-\t\tProcessing level (eg browsing vs attempting to solve problems?)
-\t\tQuality of self-explanation (knowing how activities relate to goals?)
-\t\tExploration (trying different resources)
-[@corneli2010crowdsourcing] ActiveMath as repository and community for math learning @math
-\thello
-\thi"
+lines = Array.new
+linecontext = Array.new
 
-ckey = ''
-tag = Array.new
-level = 0
-textary = Array.new
-tags = Hash.new
-a.lines.each do |l|
+# insert lines into an array with first argument being indent level, second being line content
+a.lines.each_with_index do |l, i|
+  # count tabs at front of line to get indent level
   tabs = $1 if /^(\t*)/ =~ l
-  curlevel = (defined? tabs) ? tabs.size : 0
+  level = (defined? tabs) ? tabs.size : 0
 
-  unless curlevel < level # unless there are sub-headers, process any tags and clean out current level textary
-    puts "curlevel < level"
-    if tag[curlevel]  # if there is a tag at a higher level
-      puts "tag[curlevel]"
-      text = try {textary[curlevel].join("\n").strip.remove("[@#{ckey}]")}
-      tags.add(tag[curlevel], "#{text} [@#{ckey}]\n\n")
-#      puts "Removing #{curlevel}: #{textary[curlevel]}"
-    end
-
-    textary[curlevel] = nil
-    tag[curlevel] = nil
-  end
-  (curlevel+1..textary.size-2).each {|x| textary[x] = nil } # empty all textary strings from this level and up
-
-  tag[curlevel] = $1 if /[ \n]\@(.+?)[ \n]/ =~ l  # recognize a @tag
-  puts "tag[curlevel]: #{tag[curlevel]}" if tag[curlevel]
-  ckey = $1 if /^\[@(.+?)\]/ =~ l # get citekey and keep it until it changes
-
-  puts "Curlevel #{curlevel} Level #{level}: #{l}"
-  pp "Tags: ", tags
-  pp "Tag: ", tag
-  pp "Textary: ",textary
-  puts "*" * 50
-
-  #puts "Curlevel: #{curlevel} Level #{level}"
-  #puts "adding to #{x} (which is now #{textary[x]}): #{l}";
-  (curlevel+1).times {|x| puts "adding to level #{x}";textary.add_safe(x, l)}  # add textary to all levels up to present level
-
-  level = curlevel
-#  textary.each_with_index {|x,y| puts "#{y}: #{x}"}
-#  puts "*" * 50
+  lines[i] = [level, l]
 end
 
-puts "="* 80
+# iterate through array and for each line, pick out "line context" (all subsequent lines at lower levels)
 
-tags.each {|x,y| puts "#{x}:\n#{y[0].to_s}\n#{"-"*50}\n"}
+ckey = '' # holds the current citation key
+
+lines.each_with_index do |l, i|
+
+  level = l[0]
+
+  # if first level, grab ckey or empty out
+  if level == 0
+    if /^\[@(.+?)\]/ =~ l[1]
+      ckey = "[@#{$1}]"
+    else
+      ckey = ''
+    end
+  end
+  if i == lines.size-1 || lines[i+1][0] <= level   # if it's the last entry, or the next entry is lower level
+    linecontext[i] = "#{l[1].strip} #{ckey}"
+  else
+    curlevel = level
+    c = i+1 # set start of counter to current pub
+    text = l[1].dup
+    while c < lines.size && lines[c][0] > level # as long as the level of the line in question is higher than the start level
+      curlevel, t = lines[c]
+      text << t
+      c += 1
+    end
+
+    linecontext[i] = "#{text.strip} #{ckey}"
+  end
+end
+#ap linecontext
+# iterate through array, and if tag is found, insert line context for that line into tag hash
+tags = Hash.new
+
+tag_regexp = /
+  \B                  # non-word marker
+  (?<!\[)             # not preceded by [ (to avoid catching publication references like [@publication])
+  \@(?<tagcapt>.+?)   # word starting with @
+  \b                  # word boundary
+/x
+
+lines.each_with_index do |l, i|
+  f = l[1].scan2(tag_regexp)  # recognize a @tag
+  f[:tagcapt].each {|x| tags.add(x, linecontext[i].remove(tag_regexp))} if f
+end
+
+outdir = 'litreview'
+`mkdir #{outdir}`
+tags.each do |tag, content|
+  File.write("#{outdir}/#{tag}.txt", content.join("\n\n"))
+end
+
+puts "#{tags.size} tags written to #{outdir}."
