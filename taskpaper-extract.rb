4393f996cb28da27d4ad864a358956daa4dac698
Stian Håklev <shaklev@gmail.com>
2012-04-18 23:46:16 -0400

    added header for no-citekey, and category for everything that hasn't been tagged

--- taskpaper-extract.rb
+++ taskpaper-extract.rb
@@ -20,20 +20,17 @@ end
 # iterate through array and for each line, pick out "line context" (all subsequent lines at lower levels)
 
 ckey = '' # holds the current citation key
+all_tagged = '' # holds all tagged text, to later check for lines that have not been tagged
 
+ckey_pattern = /^\[@(.+?)\]/
 lines.each_with_index do |l, i|
-
   level = l[0]
 
   # if first level, grab ckey or empty out
-  # if level == 0
-  ckey_pattern = /^\[@(.+?)\]/
-  ckey = $1 if ckey_pattern =~ l[1]
-    #   ckey = "[@#{$1}]"
-    # else
-    #   ckey = ''
-    # end
-  # end
+  if level == 0
+    ckey = (ckey_pattern =~ l[1]) ? $1 : ''
+  end
+
   if i == lines.size-1 || lines[i+1][0] <= level   # if it's the last entry, or the next entry is lower level
     linecontext[i] = [l[1].strip, ckey]
   else
@@ -50,24 +47,37 @@ lines.each_with_index do |l, i|
   end
 end
 
-# iterate through array, and if tag is found, insert line context for that line into tag hash
-tags = Hash.new
-
 tag_regexp = /
-\B                  # non-word marker
-(?<!\[)             # not preceded by [ (to avoid catching publication references like [@publication])
-\@(?<tagcapt>.+?)   # word starting with @
-\b                  # word boundary
+  \B                  # non-word marker
+  (?<!\[)             # not preceded by [ (to avoid catching publication references like [@publication])
+  \@(?<tagcapt>.+?)   # word starting with @
+  \b                  # word boundary
 /x
 
+tags = Hash.new
+
+# iterate through array, and if tag is found, insert line context for that line into tag hash
 lines.each_with_index do |l, i|
   f = l[1].scan2(tag_regexp)        # recognize a @tag
   if f                              # has tag
     f[:tagcapt].each do |x|         # for each tag if multiple
       cont = linecontext[i][0]
-      cont.remove!(tag_regexp, /\[\@#{linecontext[i][1]}\]/)
+      cont.remove!(tag_regexp, /\[\@#{linecontext[i][1]}\]/, /\:$/)
       tags.add(x, [cont.strip, linecontext[i][1]])
+      all_tagged << cont
     end
+  end
+end
+
+# do a final sweep to see if any lines have not been collected
+lines.each_with_index do |l, i|
+  next if l[1].remove(ckey_pattern).strip.size == 0       # nothing but ckey
+  next if l[1].scan2(tag_regexp)                          # recognize a @tag
+
+  cont = l[1].remove(/\[\@#{linecontext[i][1]}\]/, /\:$/).strip
+
+  unless all_tagged.index(cont) # unless it has been tagged
+    tags.add('not_tagged', [cont, linecontext[i][1]])
   end
 end
 
@@ -78,22 +88,42 @@ if ARGV[0] == 'scrivener'
   tags.each do |tag, content|
 
     out = ''
+    nockey = ''
     content.each do |fragments|
-      #fragments[0] = fragments[0].lines.map {|ln| ("\t\t" + ln).remove("\n")}
+      if fragments[1] == ''
+        nockey << "#{fragments[0]}\n\n"
+        next
+      end
       out << "#{fragments[0]} [@#{fragments[1]}]\n\n"
     end
-
+    if nockey.size > 0
+      out << nockey
+    end
     File.write("#{outdir}/#{tag}.txt", out)
   end
 
-else
+else #Taskpaper
   out = ''
+
   tags.each do |tag, content|
+    nockey = ''
     out << "#{tag}:\n"
     content.each do |fragments|
+
       fragments[0] = fragments[0].lines.map {|ln| ("\t\t" + ln).remove("\n")}
+
+      if fragments[1] == ''
+        nockey << "#{fragments[0].join("\n")}\n"
+        next
+      end
+
       out << "\t[@#{fragments[1]}]:\n#{fragments[0].join("\n")}\n"
     end
+
+    if nockey.size > 0
+      out << "\tNo citekey:\n#{nockey}"
+    end
+
   end
   File.write('out.taskpaper', out)
 end
@@ -101,8 +131,5 @@ end
 puts "#{tags.size} tags written to #{outdir}."
 
 # ideas:
-# - not tagged text, go through each line and search for it in the index of all tagged text
-#   exclude beginning of line with only [@ckeys], and lines with tags
 # - hierarchy of tags
-# - create Taskpaper document as well
 # - if tag starts with @-, only take current line
