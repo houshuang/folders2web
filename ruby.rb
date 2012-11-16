189ea634e3ad455648c214f87d1a2ba2ba3a808d
Stian Håklev <shaklev@gmail.com>
2012-11-16 12:49:57 -0500

    Updating comment, using this for newauthor and preview_iphone_image

--- ruby.rb
+++ ruby.rb
@@ -1,5 +1,9 @@
 # execute ruby script from ruby:// handler
+# currently used for newauthor (generate template for new author page) and callback from growl to preview_iphone_image
+$:.push(File.dirname($0))
+require 'utility-functions'
+
 arg = ARGV[0][7..-1]
 arg.gsub!(".rb",'')
 
-`ruby -KU /Users/stian/src/folders2web/dokuwiki.rb #{arg}`
+`ruby -KU /Users/Stian/src/folders2web/dokuwiki.rb #{arg}`
