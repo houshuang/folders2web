f392b2370d6808eeeeeb345df42fdcf19809fd52
Stian Håklev <shaklev@gmail.com>
2012-12-28 18:00:42 +0100

    Making OA-URL export to JSON more robust by explicitly converting to string

--- bibtex-batch.rb
+++ bibtex-batch.rb
@@ -127,7 +127,7 @@ b.each do |item|
   if year == "n.d." and cit.match(/\((....)\)/)
     year = $1
   end
-  json[item.key.to_s] = [namify(ax), year, cit, item.title, item[:"oa-url"]]
+  json[item.key.to_s] = [namify(ax), year, cit, item.title, item[:"oa-url"].to_s]
 
   hasfiles = Array.new
   hasfiles[2] = '' # ensure that array is filled even if some fields are empty, for alignment
